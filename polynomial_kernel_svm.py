7156f6474b6e854d9c9d97c72f618bb6a846072c
Anjali Chaurasia <anjalichaurasiakvner@gmail.com>
2026-04-21 09:53:26 +0530

    Create polynomial_kernel_svm.py

---
+++ polynomial_kernel_svm.py
@@ -0,0 +1,24 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
+
+# Dataset load karein
+data = datasets.load_breast_cancer()
+X = data.data
+y = data.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2
+)
+
+# Model initialization (Polynomial Kernel)
+model = SVC(kernel='poly', degree=3)
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+print("Polynomial Kernel Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
